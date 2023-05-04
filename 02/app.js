3323f849045a60b9f5475334a44ab92efdc0204f
JRozbicka <strawberka17@gmail.com>
2023-05-04 17:38:25 +0100

    done task 02

--- 02/app.js
+++ 02/app.js
@@ -7,9 +7,15 @@ class Counter extends React.Component {
     state = {
         amount: 0,
     }
-    
+    clickMeNow =()=> {
+        //const { amount } =this.state
+        this.setState((prevState)=>({
+           amount:prevState.amount + 1 
+        }))
+    }
     render() {
-        return <button>click me ({ this.state.amount })</button>
+        return <button
+        onClick={this.clickMeNow}>click me ({ this.state.amount })</button>
     }
 }
 
