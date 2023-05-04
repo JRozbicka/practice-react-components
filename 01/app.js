8861d5a7de98559829f26375854d66136f9a43a0
JRozbicka <strawberka17@gmail.com>
2023-05-04 10:47:52 +0100

    01 task done

--- 01/app.js
+++ 01/app.js
@@ -7,11 +7,31 @@ class App extends React.Component {
     state = {
         counter: 0,
     }
+    constructor(props) {
+        super(props)
+        console.log('construktor')
+    }
     render() {
         console.log('render');
 
         return <h1>{ this.state.counter }</h1>
     }
+    componentDidMount() {
+        this.id = setInterval(() => { 
+            const { counter } =this.state
+            console.log('zaczyana sie')
+            this.setState({ counter: counter + 1 })
+        }, 5000)
+       console.log('komponent zostal zmontowany ')
+    }
+   componentDidUpdate() {
+        console.group('komunikat po aktualizacji')
 }
+    componentWillUnmount() {
+        clearInterval(this.id)
+        console.log('posprzatane')
+    }
+}
+
 
 root.render(<App/>);
