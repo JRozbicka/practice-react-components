4e33b2c542d49721f2535615f53dc54ec7f63923
JRozbicka <strawberka17@gmail.com>
2023-05-15 13:33:18 +0100

    task3 done

--- 03/app.js
+++ 03/app.js
@@ -5,29 +5,72 @@ const root = createRoot(document.querySelector('#root'));
 
 class Article extends React.Component {
     state = {
-        comments: [],
+        
+        comments: ['test1', 'test2'],
+        text:' '
     }
+    onNewTaskChange= (e) => {
+        this.setState(() => ({
+           
+            text:e.target.value
+           
+        }))
+    }
+    addNewComment= (e)=>{
+        e.preventDefault()
+        const newComment = [
+            this.state.text
+        ]
+        this.setState((prvState)=>({
+            text:'',
+            comments:prvState.comments.concat(newComment)
+        }))
+    }
+    deleteComment =(commentId)=>{
+        console.log('delete');
+        this.setState((prevState)=>({
+            comments:prevState.comments.filter((com)=>
+                com.id !== commentId)
+        }))
+        }
+    
     
     render() {
         const {title, body} = this.props;
+        const {comments} = this.state
         return (
             <article>
                 <h1>{ title }</h1>
                 <p>{ body }</p>
                 <section>
-                    <form>
+                    <form 
+                    onSubmit={this.addNewComment}>
                         <div>
-                            <label>
+                            <label
+                            type={'text'}
+                            >
                                 <textarea 
                                     style={{ "minWidth": "300px", "minHeight": "120px" }} 
                                     name="content" 
+                                    value={this.state.text}
+                                    onChange={this.onNewTaskChange}
+                                
                                 />
                             </label>
                         </div>
-                        <div><input type="submit" value="dodaj komentarz" /></div>
+                        <div><input type="submit" 
+                        value="dodaj komentarz" 
+                        onSubmit={this.addNewComment} /></div>
                     </form>
                     <ul>
-                        {/* tutaj komentarze jako <li/>, ps. tak wygląda komentarz do kodu w JSX */}
+                        {comments.map((comment, id)=>{
+                            return (
+                                <li key={comment.id}>{comment}
+                                <button
+                                onClick={(e)=>this.deleteComment(id)}>
+                                    delete</button></li>
+                            )
+                        })}
                     </ul>
                 </section>
             </article>
